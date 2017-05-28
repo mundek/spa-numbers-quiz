fefe9d88e9a2db6ee2e09a6ed75c2e3431fa9c13
erik <eroslawsk@yahoo.com>
2017-05-27 22:31:22 -0500

    Changed for loop to while loop in randArray function and moved it out of startquiz function

--- quiz.js
+++ quiz.js
@@ -1,12 +1,10 @@
-var arr = [];
-
 document.getElementById("startBtn").addEventListener("click", startQuiz);
 function startQuiz() {
   var numberStart = 1;
   var numberEnd = 0;
   var numQuestions = 0;
   var randNumber = 0;
-  arr = [];
+  var arr = [];
   //CHECKS FOR WHICH CHECKBOX IS CHECKED THAN SETS NUMBERS FOR RANDOM FUNCTION
   if (document.getElementById("check1").checked === true) {
     numberEnd = 20;
@@ -31,18 +29,23 @@ arr = randArray();
 else {
   window.alert("Please enter amount of questions!");
 }
-  
+ } 
   //Calculates random number based on checkbox and number questions entered in input and returns array
-  function randArray() {
-    for (var i = 0; i < numQuestions; i++) {
-      randNumber = Math.floor(Math.random() * numberEnd) + numberStart;
-      if (arr.indexOf(randNumber) === -1) {
-        arr.push(randNumber);
-      } else {
-        i -= 1;
-      }
+ 
+function randArray(numberStart, numberEnd, numQuestions) {
+  var arr = [];
+  var i = 0;
+
+  while (i < numQuestions) {
+    randNumber = Math.floor(Math.random() * numberEnd) + numberStart;
+    if (arr.indexOf(randNumber) === -1) {
+      arr.push(randNumber);
+      i++;
+    } else if (numQuestions > numberEnd) {
+      arr.push(randNumber);
+      i++;
     }
-    return arr;
   }
+  return arr;
 }
 
