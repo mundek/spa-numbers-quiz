4ebb99118c93745349b66c681af73795fb3ce0cb
D221-01 <D221-01@campus.parkland.edu>
2017-06-12 18:42:19 -0500

    Minor changes.

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -4,7 +4,7 @@ theSPA.questionCounter = 0;
 theSPA.responses = [];
 
 theSPA.sayNumber = function() {
-  console.log(this.itemsArr[this.questionCounter]);
+  // console.log(this.itemsArr[this.questionCounter]);
   var numString = this.itemsArr[this.questionCounter].toString();
   responsiveVoice.speak(numString, 'Spanish Latin American Female');
 };
@@ -18,12 +18,12 @@ theSPA.advance = function() {
     + this.itemsArr.length
     + "</h2>";
   theSPA.questionCounter++;
-  console.log(this.itemsArr.length + " | " + theSPA.questionCounter);
+  // console.log(this.itemsArr.length + " | " + theSPA.questionCounter);
   
 
   if (theSPA.questionCounter >= this.itemsArr.length) {
   	// make results page visible, make quiz page invisible
-	  document.getElementById("quizPage").classList.remove('page--active');
+	document.getElementById("quizPage").classList.remove('page--active');
     document.getElementById("resultsPage").classList.add('page--active');
   	console.log(theSPA.responses);
   }
