58a570de80a8b8fc63db6ba2ad0cc42d9e822e1d
Jacob Barrett <jsbarrett88@gmail.com>
2018-03-23 15:14:00 -0500

    updated styling

--- scripts/results.js
+++ scripts/results.js
@@ -5,28 +5,34 @@ theSPA.reviewAnswer = function (num) {
   responsiveVoice.speak(numString, 'Spanish Latin American Female');
 }
 
-// container is the html ul element where it renders
-// var container = document.querySelector('.results-container');
-
-
 theSPA.reportResults = function(answersArray, responseArray) {
   let result = '';
 
   responseArray.forEach((studentResponse,i) => {
     const actualAnswer = answersArray[i];
 
-    const check = (studentResponse === actualAnswer) ?
-      `<i class="material-icons">done</i>` :
-      `! <span class="correct-answer">(${actualAnswer})</span>`;
-    result += `<li>
-      <span class="student-guess">${studentResponse}</span>
-      ${check}
-      <button onclick="theSPA.reviewAnswer(${studentResponse.toString()})">Your answer</button>
-      <button onclick="theSPA.reviewAnswer(${actualAnswer.toString()})">Correct answer</button>
-    </li>`;
+    const check = (studentResponse === actualAnswer)
+      ? `<i class="material-icons">done</i>`
+      : `! <span class="correct-answer">(${actualAnswer})</span>`;
+
+    result += `
+      <li>
+        <span class="student-guess">${studentResponse}</span>${check}
+
+        <button
+          class="review-btn review-btn--yourAnswer"
+          onclick="theSPA.reviewAnswer(${studentResponse.toString()})">
+          Your answer
+        </button>
+        <button
+          class="review-btn review-btn--correctAnswer"
+          onclick="theSPA.reviewAnswer(${actualAnswer.toString()})">
+          Correct answer
+        </button>
+      </li>
+    `;
   });
 
-
   theSPA.resContainer.innerHTML = result;
-
 }
+
